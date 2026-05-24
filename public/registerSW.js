a3694d9076b62c1ff6411afda650ecf08c31b90e
xfranklin <git.franklin@gmail.com>
2026-05-24 21:11:33 +0300

    feat: add db id

---
+++ public/registerSW.js
@@ -0,0 +1,7 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').catch(error => {
+      console.error('Service worker registration failed:', error)
+    })
+  })
+}
